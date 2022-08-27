960516c759a4904301f3822344a597328f61789d
CoderJasonJiao <112141913+CoderJasonJiao@users.noreply.github.com>
2022-08-27 23:12:47 +0800

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>
+//void Add(int* p)//每调用一次函数 变量增加1
+//{
+//	(*p)++;//传址--用指针
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("num = %d\n", num);
+//	Add(&num);
+//	printf("num = %d\n", num);
+//	Add(&num);
+//	printf("num = %d\n", num);
+//}
+
+
+//函数的嵌套调用
+//void new_line()
+//{
+//	printf("hehe\n");
+//}
+//
+//void three_line()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		new_line();
+//	}
+//}
+//
+//int main()
+//{
+//	three_line();
+//	return 0;
+//}
+
+//链式访问
+//int main()
+//{
+//	int len = 0;
+//	len = strlen("abc");
+//	printf("%d\n", len);
+//	printf("%zd\n", strlen("abc"));//链式访问
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
+
